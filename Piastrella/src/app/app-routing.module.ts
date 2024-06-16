4351b77b881e7236d342b484a37c149164e9b339
IzzoDev <aizzo450@gmail.com>
2024-06-16 23:21:26 +0200

    add homePage

--- Piastrella/src/app/app-routing.module.ts
+++ Piastrella/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/home',
+    pathMatch:'full'
+  },
+  {
+    path:'home',
+    loadChildren:() => import('./home/home.module').then(h => h.HomeModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
